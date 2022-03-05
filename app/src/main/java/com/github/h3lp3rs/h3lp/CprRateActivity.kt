a5c59c0b0be99a41de76ad92aea116739c75cca5
Ahayderi <alex.hayderi@gmail.com>
2022-03-05 17:44:27 +0100

    Implement button-activated heart animation

--- app/src/main/java/com/github/h3lp3rs/h3lp/CprRateActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/CprRateActivity.kt
@@ -2,10 +2,15 @@ package com.github.h3lp3rs.h3lp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 
-class cprRate : AppCompatActivity() {
+class CprRate : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cpr_rate)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.heartbeat_animation)
+        animation.start()
     }
 }
