0d3ec499c1bd5d4f0bfad92f4ff290e10f40d6af
eddahri <wiam.eddahri@epfl.ch>
2022-03-06 21:19:13 +0100

    Added background and top banner of the home page

--- app/src/main/java/com/github/h3lp3rs/h3lp/MainActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.EditText
 
 const val EXTRA_MESSAGE = "KEY"
@@ -11,7 +12,8 @@ const val EXTRA_MESSAGE = "KEY"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_main_page)
     }
 
     /** Called when the user taps the Send button */
---
+++ app/src/main/java/com/github/h3lp3rs/h3lp/MainPageActivity.kt
@@ -0,0 +1,20 @@
+package com.github.h3lp3rs.h3lp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hide the title bar
+        getSupportActionBar()?.hide()
+        //show the activity in full screen
+        this.getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main_page)
+    }
+}
