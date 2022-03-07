cafd57dcadeb7564d91a5c8321dcab676f656ee0
eddahri <wiam.eddahri@epfl.ch>
2022-03-07 17:54:11 +0100

    Change buttons to round style

--- app/src/main/java/com/github/h3lp3rs/h3lp/MainActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import android.widget.EditText
 
 const val EXTRA_MESSAGE = "KEY"
@@ -12,8 +11,7 @@ const val EXTRA_MESSAGE = "KEY"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main_page)
+        setContentView(R.layout.activity_main)
     }
 
     /** Called when the user taps the Send button */
