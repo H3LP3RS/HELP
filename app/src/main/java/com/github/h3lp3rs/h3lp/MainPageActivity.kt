0965a22c03fb79172d727507f4b50139899f33cb
eddahri <wiam.eddahri@epfl.ch>
2022-03-07 19:33:59 +0100

    Make the CRP rate button functional

--- app/src/main/java/com/github/h3lp3rs/h3lp/MainPageActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/MainPageActivity.kt
@@ -1,9 +1,11 @@
 package com.github.h3lp3rs.h3lp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.Window
+import android.view.View
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,11 @@ class MainPageActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main_page)
+    }
+
+    /** Called when the user taps the cpr rate button */
+    fun goToCprActivity(view: View) {
+        val intent = Intent(this, CprRateActivity::class.java)
+        startActivity(intent)
     }
 }
