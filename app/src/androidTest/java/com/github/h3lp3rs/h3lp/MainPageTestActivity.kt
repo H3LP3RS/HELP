e4c79950c9055e928884a7ff2f4bc862e1268df0
eddahri <wiam.eddahri@epfl.ch>
2022-03-08 20:52:12 +0100

    Add test: cpr button correctly starts activity

---
+++ app/src/androidTest/java/com/github/h3lp3rs/h3lp/MainPageTestActivity.kt
@@ -0,0 +1,47 @@
+package com.github.h3lp3rs.h3lp
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import android.text.method.Touch.scrollTo
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainPageTestActivity {
+    @get:Rule
+    val testRule = ActivityScenarioRule(
+        MainPageActivity::class.java
+    )
+    @Test
+    fun clickingOnCPRButtonWorksAndSendsIntent() {
+        Intents.init()
+        val intent = Intent()
+        val intentResult = Instrumentation.ActivityResult(Activity.RESULT_OK, intent)
+        Intents.intending(IntentMatchers.anyIntent()).respondWith(intentResult)
+
+        onView(withId(R.id.CPR_rate_button)).perform(ViewActions.scrollTo(), click());
+
+        Intents.intended(
+            Matchers.allOf(
+                IntentMatchers.hasComponent(CprRateActivity::class.java.name))
+            )
+        Intents.release()
+    }
+
+
+
+}
