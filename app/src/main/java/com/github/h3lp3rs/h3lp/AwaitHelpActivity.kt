7eeb04012e0508476ed3c46042fb36058d4dfa27
Emilien <emilien.duc@epfl.ch>
2022-03-09 15:45:52 +0100

    adds awaitHelpActivity test

--- app/src/main/java/com/github/h3lp3rs/h3lp/AwaitHelpActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/AwaitHelpActivity.kt
@@ -2,6 +2,8 @@ package com.github.h3lp3rs.h3lp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 
 class AwaitHelpActivity : AppCompatActivity() {
@@ -11,6 +13,12 @@ class AwaitHelpActivity : AppCompatActivity() {
 
         val bundle = this.intent.extras
         val array = bundle!!.getStringArrayList(EXTRA_NEEDED_MEDICATION)
+
+        // To be removed once the page is implemented:
+        val displayText = "Selected: $array"
+        val text = findViewById<TextView>(R.id.selected_items_text).apply{
+            text = displayText
+        }
 
         //TODO Launch helper search
         // a: Retrieve nearby users from online database            => loading bar (up to 50%
