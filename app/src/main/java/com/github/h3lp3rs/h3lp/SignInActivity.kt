819d411a91da9f734ff8d9115516d4151d38ee0d
HindEl <hind.elbouchrifi@epfl.ch>
2022-03-09 14:42:04 +0100

    replace deprecated methods

--- app/src/main/java/com/github/h3lp3rs/h3lp/SignInActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/SignInActivity.kt
@@ -1,10 +1,12 @@
 package com.github.h3lp3rs.h3lp
 
+import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowInsetsController
 import android.view.WindowManager
 import android.widget.ImageButton
 import android.widget.Toast
@@ -17,15 +19,13 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlin.math.sign
 
 class SignInActivity : AppCompatActivity() {
 
-    companion object{
-        private const val RC_SIGN_IN = 19
-    }
-
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
+    private val serverClientId = "899579782202-t3orsbp6aov3i91c99r72kc854og8jad.apps.googleusercontent.com"
 
     override fun onStart() {
         super.onStart()
@@ -57,7 +57,7 @@ class SignInActivity : AppCompatActivity() {
     private fun makeRequest() {
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("899579782202-t3orsbp6aov3i91c99r72kc854og8jad.apps.googleusercontent.com")
+            .requestIdToken(serverClientId)
             .requestEmail()
             .build()
 
@@ -66,26 +66,25 @@ class SignInActivity : AppCompatActivity() {
 
     private fun signIn() {
         val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
+        resultLauncher.launch(signInIntent)
+        //startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                // Google Sign In failed, display a message to the user.
-                Log.w(TAG, "Google sign in failed", e)
-                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, display a message to the user.
+                    Log.w(TAG, "Google sign in failed", e)
+                    Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                }
             }
-        }
     }
 
     private fun firebaseAuthWithGoogle(idToken: String) {
