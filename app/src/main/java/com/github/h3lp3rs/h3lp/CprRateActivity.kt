047f719084850748ae45d94779847504834ca955
Ahayderi <alex.hayderi@gmail.com>
2022-03-10 09:22:53 +0100

    Add comments explaining animations and the CPR rate

--- app/src/main/java/com/github/h3lp3rs/h3lp/CprRateActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/CprRateActivity.kt
@@ -23,6 +23,26 @@ class CprRateActivity : AppCompatActivity() {
         val beepSound = MediaPlayer.create(this, R.raw.beep)
         beepSound.isLooping = true
 
+        /**
+         * Note: The animation of the heartbeat is currently commented out since
+         *      espresso tests freeze if animations are running. I haven't yet
+         *      found a way to only disable such animations at test time (these aren't the same
+         *      as the ones that can be disabled in Android developer settings).
+         */
+
+        /**
+         * The activity essentially has 2 states:
+         * 1. Nothing is happening, the button text is set to "START"
+         * 2. If the button is clicked, the animation of a heartbeat starts playing
+         *    with an accompanying sound, the button text is set to "STOP"
+         *
+         * The heartbeat animation/sound run at 107 beats/beeps per minute since this was
+         * found to be the optimal CPR rate
+         * Source: Duval S, Pepe PE, Aufderheide TP, et al. Optimal Combination of Compression Rate
+         * and Depth During Cardiopulmonary Resuscitation for Functionally Favorable Survival.
+         * JAMA Cardiol. 2019;4(9):900–908. doi:10.1001/jamacardio.2019.2717
+         */
+
         startButton.setOnClickListener {
             if (animationIsPlaying) {
 //                heartIcon.clearAnimation()
