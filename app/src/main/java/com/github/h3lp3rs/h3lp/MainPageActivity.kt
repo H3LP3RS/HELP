f0c593fec8768c5d3cc3cb1daaaa1f18d8f917de
eddahri <wiam.eddahri@epfl.ch>
2022-03-10 13:40:08 +0100

    Remove the full screen mode

--- app/src/main/java/com/github/h3lp3rs/h3lp/MainPageActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/MainPageActivity.kt
@@ -3,19 +3,12 @@ package com.github.h3lp3rs.h3lp
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Show the activity in full screen
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
         setContentView(R.layout.activity_main_page)
     }
 
