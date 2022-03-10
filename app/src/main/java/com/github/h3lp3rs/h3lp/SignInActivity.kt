cd398ecd32e5f1565f0d1f0263c733ef140d01c8
HindEl <hind.elbouchrifi@epfl.ch>
2022-03-10 13:25:42 +0100

    add review modifications

--- app/src/main/java/com/github/h3lp3rs/h3lp/SignInActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/SignInActivity.kt
@@ -82,11 +82,9 @@ class SignInActivity : AppCompatActivity() {
                 try {
                     // Google Sign In was successful, authenticate with Firebase
                     val account = task.getResult(ApiException::class.java)!!
-                    Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
                     firebaseAuthWithGoogle(account.idToken!!)
                 } catch (e: ApiException) {
                     // Google Sign In failed, display a message to the user.
-                    Log.w(TAG, "Google sign in failed", e)
                     Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
                 }
             }
@@ -103,12 +101,10 @@ class SignInActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update activity with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
                     val intent = Intent(this, HomeActivity::class.java)
                     startActivity(intent)
                 } else {
                     // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
                     Toast.makeText(this, "Sorry authentication failed.", Toast.LENGTH_SHORT).show()
                 }
             }
