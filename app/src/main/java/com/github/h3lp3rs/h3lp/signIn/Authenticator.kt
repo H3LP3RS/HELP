afc5ea101e629c8067350e29314631788ba91155
Ahayderi <alex.hayderi@gmail.com>
2022-03-15 10:59:27 +0100

    Add interface to enable sign in mocking

---
+++ app/src/main/java/com/github/h3lp3rs/h3lp/signIn/Authenticator.kt
@@ -0,0 +1,12 @@
+package com.github.h3lp3rs.h3lp.signIn
+
+object Authenticator {
+    private var currentAuthenticator: AuthenticatorInterface? = null
+    fun  get(): AuthenticatorInterface {
+        currentAuthenticator = currentAuthenticator ?: FirebaseAuthAdaptor
+        return currentAuthenticator!!
+    }
+    fun set(customAuthenticator: AuthenticatorInterface) {
+        currentAuthenticator = customAuthenticator
+    }
+}
--- app/src/main/java/com/github/h3lp3rs/h3lp/signIn/AuthenticatorInterface.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/signIn/AuthenticatorInterface.kt
@@ -1,13 +1,4 @@
 package com.github.h3lp3rs.h3lp.signIn
 
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthCredential
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseUser
-
 interface AuthenticatorInterface {
-
-    fun getCurrentUser(): FirebaseUser?
-
-    fun signInWithCredential(credential: AuthCredential): Task<AuthResult?>?
 }
