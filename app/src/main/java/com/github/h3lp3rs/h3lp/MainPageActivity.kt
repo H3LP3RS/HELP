23dd110dd66b7c620b92feef1aba42cf25d94feb
eddahri <wiam.eddahri@epfl.ch>
2022-03-17 18:52:40 +0100

    Remove toast feature

--- app/src/main/java/com/github/h3lp3rs/h3lp/MainPageActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/MainPageActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
-import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -40,7 +38,6 @@ class MainPageActivity : AppCompatActivity() {
                     GravityCompat.START
                 )
                 else -> {
-                    Toast.makeText(applicationContext, "TODO", LENGTH_SHORT).show()
                     true
                 }
             }
