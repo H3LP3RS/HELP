246f2afc57b120b3e07b28203f2d315717dbb116
eddahri <wiam.eddahri@epfl.ch>
2022-03-17 18:30:27 +0100

    Refactor Main Page activity

--- app/src/main/java/com/github/h3lp3rs/h3lp/MainPageActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/MainPageActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -14,26 +16,10 @@ import com.google.android.material.navigation.NavigationView
 class MainPageActivity : AppCompatActivity() {
     private lateinit var toggle: ActionBarDrawerToggle
 
-    /*
-    val drawerLayout: DrawerLayout=findViewById<DrawerLayout>(R.layout.activity_main_page)
-    val navigationView:NavigationView=findViewById(R.id.nav_view)
-    val toolBar:ImageButton=findViewById(R.id.menuButton)
-    val drawerToggle by lazy{
-        ActionBarDrawerToggle(this,findViewById(R.id.bar_layout),findViewById(R.id.toolbar),R.string.drawer_open,R.string.drawer_closed)
-    }
-    private var mDrawerToggle: ActionBarDrawerToggle?=null
-    */
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
-        /*
-        setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<DrawerLayout>(R.id.bar_layout).addDrawerListener(drawerToggle)
-        mDrawerToggle= ActionBarDrawerToggle(this,findViewById<DrawerLayout>(R.id.drawer_layout),findViewById(R.id.toolbar),R.string.drawer_open,R.string.drawer_closed)
-        mDrawerToggle!!.syncState()
-        */
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -51,7 +37,7 @@ class MainPageActivity : AppCompatActivity() {
                     GravityCompat.START
                 )
                 else -> {
-                    // Toast.makeText(applicationContext, "TODO", LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "TODO", LENGTH_SHORT).show()
                     true
                 }
             }
@@ -59,18 +45,6 @@ class MainPageActivity : AppCompatActivity() {
         }
 
     }
-
-    /*
-        override fun onPostCreate(savedInstanceState: Bundle?) {
-            super.onPostCreate(savedInstanceState)
-            drawerToggle.syncState()
-        }
-
-        override fun onConfigurationChanged(newConfig: Configuration) {
-            super.onConfigurationChanged(newConfig)
-            drawerToggle.onConfigurationChanged(newConfig)
-        }
-    */
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (toggle.onOptionsItemSelected(item)) true else super.onOptionsItemSelected(item)
