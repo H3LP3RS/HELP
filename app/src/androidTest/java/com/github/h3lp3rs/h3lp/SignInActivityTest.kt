7c9048d9474cd7d453db10c70038110789d7b1bc
unknown <alexis.schlomer@epfl.ch>
2022-03-17 19:19:11 +0100

    Adapt sign-in test and make it compatible with ToS change

--- app/src/androidTest/java/com/github/h3lp3rs/h3lp/SignInActivityTest.kt
+++ app/src/androidTest/java/com/github/h3lp3rs/h3lp/SignInActivityTest.kt
@@ -1,29 +1,38 @@
 package com.github.h3lp3rs.h3lp
 
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.Intents.*
+import androidx.test.espresso.intent.matcher.IntentMatchers.*
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Rule
+import com.github.h3lp3rs.h3lp.preferences.Preferences
+import com.github.h3lp3rs.h3lp.preferences.Preferences.Companion.Files.*
+import com.github.h3lp3rs.h3lp.preferences.Preferences.Companion.USER_AGREE
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SignInActivityTest {
-    @get:Rule
-    val testRule = ActivityScenarioRule(
-        SignInActivity::class.java
-    )
+    @Before
+    fun clearPreferences() {
+        Preferences.clearAllPreferences(ApplicationProvider.getApplicationContext())
+    }
 
     @Test
     fun loginButtonLaunchesIntent() {
-        Intents.init()
-        onView(withId(R.id.signInButton)).check(matches(isDisplayed())).perform(ViewActions.click())
-        Intents.intended(IntentMatchers.hasPackage("com.google.android.gms"))
-        Intents.release()
+        Preferences(PRESENTATION, ApplicationProvider.getApplicationContext()).setBool(USER_AGREE, true)
+        val intent = Intent(ApplicationProvider.getApplicationContext(), SignInActivity::class.java)
+        ActivityScenario.launch<SignInActivity>(intent).use {
+            init()
+            onView(withId(R.id.signInButton)).check(matches(isDisplayed())).perform(ViewActions.click())
+            intended(hasPackage("com.google.android.gms"))
+            release()
+        }
     }
 }
