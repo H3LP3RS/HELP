b1d58405ec1559f8eb45b2bbd04aaac92b6b7197
eddahri <wiam.eddahri@epfl.ch>
2022-03-31 13:14:50 +0200

    Fix minor typo

--- app/src/androidTest/java/com/github/h3lp3rs/h3lp/GuideTest.kt
+++ app/src/androidTest/java/com/github/h3lp3rs/h3lp/GuideTest.kt
@@ -5,7 +5,10 @@ import androidx.preference.PreferenceManager
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
