ab3169ec3e7878a79aaea7d8dd5f08ae78ad82af
unknown <alexis.schlomer@epfl.ch>
2022-04-07 15:28:40 +0200

    Minor refactoring in test presentation classes

--- app/src/androidTest/java/com/github/h3lp3rs/h3lp/presentation/SwipeListenerTest.kt
+++ app/src/androidTest/java/com/github/h3lp3rs/h3lp/presentation/SwipeListenerTest.kt
@@ -4,6 +4,7 @@ import android.view.MotionEvent
 import androidx.test.core.view.MotionEventBuilder.*
 import com.github.h3lp3rs.h3lp.listeners.SwipeListener
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -30,45 +31,46 @@ class SwipeListenerTest {
     @Test
     fun swipeRightIsTriggered() {
         val swiperListener = SwipeListener({++counter}, {}, {}, {})
-        val motionEvent2 = motionEvent(swipeThreshold + 1, 0f)
-        val motionEvent1 = motionEvent(0f, 0f)
-        swiperListener.onFling(motionEvent1, motionEvent2, swipeVelocityThreshold + 1, 0f)
+        swiperListener.onFling(motionEvent(0f, 0f), motionEvent(swipeThreshold + 1, 0f),
+            swipeVelocityThreshold + 1, 0f)
         assertEquals(1, counter)
     }
 
     @Test
     fun swipeLeftIsTriggered() {
         val swiperListener = SwipeListener({}, {++counter}, {}, {})
-        val motionEvent2 = motionEvent(0f, 0f)
-        val motionEvent1 = motionEvent(swipeThreshold + 1, 0f)
-        swiperListener.onFling(motionEvent1, motionEvent2, swipeVelocityThreshold + 1, 0f)
+        swiperListener.onFling(motionEvent(swipeThreshold + 1, 0f), motionEvent(0f, 0f),
+            swipeVelocityThreshold + 1, 0f)
         assertEquals(1, counter)
     }
 
     @Test
     fun swipeUpIsTriggered() {
         val swiperListener = SwipeListener({}, {}, {++counter}, {})
-        val motionEvent2 = motionEvent(0f, 0f)
-        val motionEvent1 = motionEvent(0f, swipeThreshold + 1)
-        swiperListener.onFling(motionEvent1, motionEvent2, 0f, swipeVelocityThreshold + 1)
+        swiperListener.onFling(motionEvent(0f, swipeThreshold + 1), motionEvent(0f, 0f),
+            0f, swipeVelocityThreshold + 1)
         assertEquals(1, counter)
     }
 
     @Test
     fun swipeDownIsTriggered() {
         val swiperListener = SwipeListener({}, {}, {}, {++counter})
-        val motionEvent2 = motionEvent(0f, swipeThreshold + 1)
-        val motionEvent1 = motionEvent(0f, 0f)
-        swiperListener.onFling(motionEvent1, motionEvent2, 0f, swipeVelocityThreshold + 1)
+        swiperListener.onFling(motionEvent(0f, 0f), motionEvent(0f, swipeThreshold + 1),
+            0f, swipeVelocityThreshold + 1)
         assertEquals(1, counter)
     }
 
     @Test
     fun noSwipeDoesNotTrigger() {
         val swiperListener = SwipeListener({++counter}, {++counter}, {++counter}, {++counter})
-        val motionEvent2 = motionEvent(0f, 0f)
-        val motionEvent1 = motionEvent(0f, 0f)
-        swiperListener.onFling(motionEvent1, motionEvent2, 0f, 0f)
+        swiperListener.onFling(motionEvent(0f, 0f), motionEvent(0f, 0f),
+            0f, 0f)
         assertEquals(0, counter)
+    }
+
+    @Test
+    fun onDownReturnsTrue() {
+        val swiperListener = SwipeListener({}, {}, {}, {})
+        assertTrue(swiperListener.onDown(motionEvent(0f, 0f)))
     }
 }
