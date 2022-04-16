1ecf8b2cbf001072314a702badb7ef8924302910
Ahayderi <alex.hayderi@gmail.com>
2022-04-16 11:07:30 +0200

    Create basic objects for messaging

---
+++ app/src/main/java/com/github/h3lp3rs/h3lp/messaging/Message.kt
@@ -0,0 +1,4 @@
+package com.github.h3lp3rs.h3lp.messaging
+
+class Message {
+}
---
+++ app/src/main/java/com/github/h3lp3rs/h3lp/messaging/TestMessagingActivity.kt
@@ -0,0 +1,12 @@
+package com.github.h3lp3rs.h3lp.messaging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class testMessagingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_messaging)
+
+    }
+}
