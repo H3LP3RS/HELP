73b145ca8c027d367d825452b1e7364de50ab4da
Hind <hind.elbouchrifi@gmail.com>
2022-04-22 14:24:16 +0200

    add pro main tests and remove status selection activity

---
+++ app/src/androidTest/java/com/github/h3lp3rs/h3lp/professional/ProMainActivityTest.kt
@@ -0,0 +1,39 @@
+package com.github.h3lp3rs.h3lp.professional
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.h3lp3rs.h3lp.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ProMainActivityTest {
+
+    @get:Rule
+    val testRule = ActivityScenarioRule(
+        ProMainActivity::class.java
+    )
+
+    private fun checkIfDisplayed(id: Int){
+        Espresso.onView(ViewMatchers.withId(id))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun textsAreDisplayed(){
+        checkIfDisplayed(R.id.HelloText)
+        checkIfDisplayed(R.id.welcomeText)
+    }
+
+    @Test
+    fun buttonsAreDisplayed(){
+        checkIfDisplayed(R.id.faq_button)
+        checkIfDisplayed(R.id.pro_profile_button)
+        checkIfDisplayed(R.id.emergencies_button)
+        checkIfDisplayed(R.id.blood_request_button)
+    }
+}
