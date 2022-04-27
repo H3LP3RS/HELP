1e69c41965e42d14421f34f90ff76f34d56425d3
unknown <alexis.schlomer@epfl.ch>
2022-04-27 16:54:04 +0200

    Add test for emergency rejection

--- app/src/androidTest/java/com/github/h3lp3rs/h3lp/HelpPageActivityTest.kt
+++ app/src/androidTest/java/com/github/h3lp3rs/h3lp/HelpPageActivityTest.kt
@@ -1,6 +1,9 @@
 package com.github.h3lp3rs.h3lp
 
 import android.Manifest
+import android.app.Activity
+import android.app.Instrumentation
+import android.app.Instrumentation.*
 import android.content.Intent
 import android.location.Location
 import android.os.Bundle
@@ -10,6 +13,10 @@ import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.*
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.matcher.IntentMatchers.*
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -29,7 +36,8 @@ import com.github.h3lp3rs.h3lp.storage.Storages
 import com.github.h3lp3rs.h3lp.storage.Storages.Companion.resetStorage
 import com.github.h3lp3rs.h3lp.storage.Storages.Companion.storageOf
 import junit.framework.TestCase.assertTrue
-import org.hamcrest.Matchers.containsString
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -60,6 +68,7 @@ class HelpPageActivityTest {
         mock(LocationManagerInterface::class.java)
     private val locationMock: Location = mock(Location::class.java)
     private val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    private val helpId = 1
 
 
     @get:Rule
@@ -143,8 +152,33 @@ class HelpPageActivityTest {
 
     @Test
     fun acceptingAnEmergencyNotifiesTheHelpee() {
+        setupEmergencyAndDo {
+            // Accept
+            onView(withId(R.id.accept)).perform(click())
+            val updatedEmergency = databaseOf(EMERGENCIES).getObject(helpId.toString(), EmergencyInformation::class.java)
+            val helpers = ArrayList(updatedEmergency.get().helpers)
+            // Check the helper has been added to the emergency object
+            assertTrue(helpers[0].uid == USER_TEST_ID)
+        }
+    }
+
+    @Test
+    fun refusingAnEmergencyGoesBackToMainPage() {
+        setupEmergencyAndDo {
+            Intents.init()
+            val i = Intent()
+            val intentResult = ActivityResult(Activity.RESULT_OK, i)
+            intending(anyIntent()).respondWith(intentResult)
+            // Reject
+            onView(withId(R.id.reject)).perform(click())
+            intended(allOf(hasComponent(MainPageActivity::class.java.name)))
+            release()
+        }
+    }
+
+    /* Forge an emergency situation */
+    private fun setupEmergencyAndDo(action: () -> Unit) {
         // Forge the right intent
-        val helpId = 1
         val bundle = Bundle()
         bundle.putString(EXTRA_EMERGENCY_KEY, helpId.toString())
         bundle.putStringArrayList(EXTRA_HELP_REQUIRED_PARAMETERS, arrayListOf(EPIPEN))
@@ -163,22 +197,16 @@ class HelpPageActivityTest {
         PREFERENCES.db = MockDatabase()
         val emergencyDb = MockDatabase()
         val skills = HelperSkills(true, true, true, true,
-                                    true, true)
+            true, true)
         val emergency = EmergencyInformation(helpId.toString(), 2.0, 2.0, skills,
-                            ArrayList(listOf("Epipen")), Date(), null, ArrayList())
+            ArrayList(listOf("Epipen")), Date(), null, ArrayList())
         emergencyDb.setObject(helpId.toString(), EmergencyInformation::class.java, emergency)
         EMERGENCIES.db = emergencyDb
         // Setup skills storage accordingly
         storageOf(Storages.SKILLS).setObject(globalContext.getString(R.string.my_skills_key),
             HelperSkills::class.java, skills)
-        // Simulate click on notification
         launch<HelpPageActivity>(intent).use {
-            // Accept
-            onView(withId(R.id.accept)).perform(click())
-            val updatedEmergency = databaseOf(EMERGENCIES).getObject(helpId.toString(), EmergencyInformation::class.java)
-            val helpers = ArrayList(updatedEmergency.get().helpers)
-            // Check the helper has been added to the emergency object
-            assertTrue(helpers[0].uid == USER_TEST_ID)
+            action()
         }
     }
 }
