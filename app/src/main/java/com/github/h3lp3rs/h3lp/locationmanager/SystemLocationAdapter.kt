48831b996fdf2e8c5c5dfb0467d6121aa9f20e53
Ahayderi <alex.hayderi@gmail.com>
2022-04-30 21:28:51 +0200

    Add more documentation

--- app/src/main/java/com/github/h3lp3rs/h3lp/locationmanager/SystemLocationAdapter.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/locationmanager/SystemLocationAdapter.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.core.content.ContextCompat
+import com.github.h3lp3rs.h3lp.locationmanager.LocationManagerInterface.Companion.GET_LOCATION_EXCEPTION
+import com.github.h3lp3rs.h3lp.locationmanager.LocationManagerInterface.Companion.GET_PERMISSIONS_EXCEPTION
 import com.google.android.gms.location.LocationServices
 import java.util.concurrent.CompletableFuture
 
@@ -16,8 +18,6 @@ import java.util.concurrent.CompletableFuture
  * the app
  */
 object SystemLocationAdapter : LocationManagerInterface {
-    private const val GET_LOCATION_EXCEPTION = "Location could not be retrieved"
-    private const val GET_PERMISSIONS_EXCEPTION = "Location permissions were not granted"
 
     /**
      * Gets the user's current location from the system location manager
@@ -32,12 +32,15 @@ object SystemLocationAdapter : LocationManagerInterface {
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
+            // A fused location provider uses both GPS and Network data to get an accurate
+            // location estimation without requiring too much battery use
             val fusedLocationProviderClient =
                 LocationServices.getFusedLocationProviderClient(context)
             fusedLocationProviderClient.lastLocation
                 .addOnSuccessListener { futureLocation.complete(it) }
                 .addOnFailureListener { futureLocation.completeExceptionally(RuntimeException(GET_LOCATION_EXCEPTION)) }
         } else {
+            // In case the user didn't give location permissions / removed them
             futureLocation.completeExceptionally(RuntimeException(GET_PERMISSIONS_EXCEPTION))
         }
         return futureLocation
