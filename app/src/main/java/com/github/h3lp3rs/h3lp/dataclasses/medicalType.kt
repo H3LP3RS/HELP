7ea5d186a6f6a323476c8a02ab23decdb7024019
Adrinels <56875743+Adrinels@users.noreply.github.com>
2022-05-01 17:02:18 +0200

    add medicalType.kt dataclass

---
+++ app/src/main/java/com/github/h3lp3rs/h3lp/dataclasses/medicalType.kt
@@ -0,0 +1,4 @@
+package com.github.h3lp3rs.h3lp.dataclasses
+
+data class medicalType( val generalist: Boolean, val cardiology: Boolean, val traumatology: Boolean, val pediatry: Boolean,
+                        val neurology: Boolean, val gynecology: Boolean)
