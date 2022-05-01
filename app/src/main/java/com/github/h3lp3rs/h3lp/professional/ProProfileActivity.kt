f9e2868d62efe740f39c9d5b170160715261c415
Hind <hind.elbouchrifi@gmail.com>
2022-05-01 15:34:16 +0200

    Add empty activity for profile page

---
+++ app/src/main/java/com/github/h3lp3rs/h3lp/professional/ProProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.github.h3lp3rs.h3lp.professional
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.h3lp3rs.h3lp.R
+
+class ProProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pro_profile)
+    }
+}
