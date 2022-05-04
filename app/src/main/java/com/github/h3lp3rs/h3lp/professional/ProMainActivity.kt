f4d24cec93046cde8da3f3665ffa6ae0f2e01347
Hind <hind.elbouchrifi@gmail.com>
2022-05-04 14:35:14 +0200

    Add policy rules

--- app/src/main/java/com/github/h3lp3rs/h3lp/professional/ProMainActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/professional/ProMainActivity.kt
@@ -1,7 +1,9 @@
 package com.github.h3lp3rs.h3lp.professional
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.github.h3lp3rs.h3lp.R
 
 /**
@@ -13,5 +15,16 @@ class ProMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_professional_main)
     }
+
+    /** Starts the activity by sending intent */
+    private fun goToActivity(ActivityName : Class<*>?) {
+        val intent = Intent(this, ActivityName)
+        startActivity(intent)
+    }
+
+    fun goToProProfileActivity(view : View){
+        goToActivity(ProProfileActivity::class.java)
+    }
+
 
 }
--- app/src/main/java/com/github/h3lp3rs/h3lp/professional/ProProfileActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/professional/ProProfileActivity.kt
@@ -2,11 +2,66 @@ package com.github.h3lp3rs.h3lp.professional
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.TextUtils
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.github.h3lp3rs.h3lp.R
 
 class ProProfileActivity : AppCompatActivity() {
+    private var proStatus = ""
+    private var proDomain = ""
+    private var proExperience = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pro_profile)
+
+        createPrivacyCheckBox()
     }
+    /**
+     * Create a PrivacyCheckbox with a clickable link sending to the policy
+     */
+    private fun createPrivacyCheckBox() {
+        val checkBox = findViewById<CheckBox>(R.id.ProProfilePrivacyCheck)
+
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                // This avoids checkbox checking when clicking on the policy link
+                // Sadly it is incompatible with Expresso onClick() so is commented for cirrus
+                // widget.cancelPendingInputEvents()
+                showPolicy()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true
+            }
+        }
+        val linkText = SpannableString(getString(R.string.privacy_policy))
+        linkText.setSpan(clickableSpan, 0, linkText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        val cs = TextUtils.expandTemplate("I accept the ^1", linkText)
+        checkBox.text = cs
+        checkBox.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun showPolicy() {
+        AlertDialog.Builder(this).setTitle(getString(R.string.privacy_policy))
+            .setMessage(getString(R.string.pro_profile_privacy_policy)).show()
+    }
+
+    private fun loadData(){
+        proStatus = findViewById<EditText>(R.id.proProfileStatusEditTxt).text.toString()
+        proDomain = findViewById<EditText>(R.id.proProfileDomainEditTxt).text.toString()
+        proExperience = findViewById<EditText>(R.id.ProProfileExperienceEditTxt).text.toString().toInt()
+        
+    }
+
+
 }
