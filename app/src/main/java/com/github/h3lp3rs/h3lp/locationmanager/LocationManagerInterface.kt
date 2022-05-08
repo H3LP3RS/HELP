9e2448ae89d095cb2420126cac2752b3476ff1d5
unknown <alexis.schlomer@epfl.ch>
2022-05-08 18:28:00 +0200

    Implement distance filtering

--- app/src/main/java/com/github/h3lp3rs/h3lp/locationmanager/LocationManagerInterface.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/locationmanager/LocationManagerInterface.kt
@@ -2,6 +2,8 @@ package com.github.h3lp3rs.h3lp.locationmanager
 
 import android.content.Context
 import android.location.Location
+import com.github.h3lp3rs.h3lp.signin.SignInActivity
+import kotlin.math.*
 
 /**
  * General interface displaying the methods required from a location manager in the context of the
@@ -15,4 +17,46 @@ interface LocationManagerInterface {
      * permissions
      */
     fun getCurrentLocation(context: Context): Location?
+
+    /**
+     * Returns the distance in meters from the user to the target coordinates, nullable
+     * if the user cannot be located
+     * @param coordinates The coordinates (latitude, longitude) of the target
+     * @param context The activity from which the location manager is called to get
+     * the user's permissions
+     * @return distance (in meters)
+     */
+    fun distanceFrom(coordinates: Pair<Double, Double>, context: Context): Double? {
+        getCurrentLocation(context)?.let {
+            return getDistanceFromLatLon(Pair(it.latitude, it.longitude), coordinates)
+        }
+        return null
+    }
+
+    /**
+     * Calculates the distance in meters between two lat/lon points on Earth
+     * Code adapted from https://forum.mendix.com/link/questions/6986
+     * @param coordinates1
+     * @param coordinates2
+     * @return distance (in meters)
+     */
+    private fun getDistanceFromLatLon(
+        coordinates1: Pair<Double, Double>,
+        coordinates2: Pair<Double, Double>
+    ): Double {
+        val (lat1, lon1) = coordinates1
+        val (lat2, lon2) = coordinates2
+        val r = 6371 // Radius of the earth in km
+        val dLat = deg2rad(lat2 - lat1)
+        val dLon = deg2rad(lon2 - lon1)
+        val a = sin(dLat / 2) * sin(dLat / 2) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) *
+                sin(dLon / 2) * sin(dLon / 2)
+
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return 1000 * r * c
+    }
+
+    private fun deg2rad(deg: Double): Double {
+        return deg * (PI / 180)
+    }
 }
--- app/src/main/java/com/github/h3lp3rs/h3lp/notification/EmergencyListener.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/notification/EmergencyListener.kt
@@ -4,39 +4,66 @@ import android.content.Intent
 import android.os.Bundle
 import com.github.h3lp3rs.h3lp.*
 import com.github.h3lp3rs.h3lp.database.Databases
+import com.github.h3lp3rs.h3lp.database.Databases.Companion.databaseOf
 import com.github.h3lp3rs.h3lp.dataclasses.EmergencyInformation
 import com.github.h3lp3rs.h3lp.dataclasses.HelperSkills
+import com.github.h3lp3rs.h3lp.locationmanager.GeneralLocationManager
+import com.github.h3lp3rs.h3lp.notification.NotificationService.Companion.createNotificationChannel
+import com.github.h3lp3rs.h3lp.notification.NotificationService.Companion.sendIntentNotification
 import com.github.h3lp3rs.h3lp.signin.SignInActivity
+import com.github.h3lp3rs.h3lp.signin.SignInActivity.Companion.globalContext
 import com.github.h3lp3rs.h3lp.storage.Storages
+import com.github.h3lp3rs.h3lp.storage.Storages.Companion.storageOf
 
 object EmergencyListener {
+
+    // Maximum helpee-helper distance in meters
+    private const val MAX_DISTANCE = 1000
 
     /**
      * Activates all listeners for helpees the helper may help
      */
     fun activateListeners() {
-        val skillStorage = Storages.storageOf(Storages.SKILLS)
-        val skills = skillStorage.getObjectOrDefault(SignInActivity.globalContext.getString(R.string.my_skills_key), HelperSkills::class.java, null)
-        if(skills == null) return
+        val skillStorage = storageOf(Storages.SKILLS)
+        val skills = skillStorage.getObjectOrDefault(
+            SignInActivity.globalContext.getString(R.string.my_skills_key),
+            HelperSkills::class.java,
+            null
+        )
+        if (skills == null) return
         else {
-            val db = Databases.databaseOf(Databases.NEW_EMERGENCIES)
+            val db = databaseOf(Databases.NEW_EMERGENCIES)
+
             // Utility function to add a specific listener to a specific key if present
             fun activateListener(isPresent: Boolean, key: String) {
-                if(isPresent) {
+                if (isPresent) {
                     db.addListenerIfNotPresent(key, Int::class.java) { id ->
                         // Look if we already encountered this id
-                        val emergencyStorage = Storages.storageOf(Storages.EMERGENCIES_RECEIVED)
-                        if(emergencyStorage.getBoolOrDefault(id.toString(), false)) {
+                        val emergencyStorage = storageOf(Storages.EMERGENCIES_RECEIVED)
+                        if (emergencyStorage.getBoolOrDefault(id.toString(), false)) {
                             return@addListenerIfNotPresent
                         }
                         // Never see this emergency again later
                         emergencyStorage.setBoolean(id.toString(), true)
+
                         // Send notification only if the associated object still exists
-                        Databases.databaseOf(Databases.EMERGENCIES)
+                        databaseOf(Databases.EMERGENCIES)
                             .getObject(id.toString(), EmergencyInformation::class.java)
                             .thenAccept {
-                                NotificationService.createNotificationChannel(SignInActivity.globalContext)
-                                val intent = Intent(SignInActivity.globalContext, HelperPageActivity::class.java)
+                                // Distance filtering
+                                val distance = GeneralLocationManager.get()
+                                    .distanceFrom(Pair(it.latitude, it.longitude), globalContext)
+                                if(distance == null || distance > MAX_DISTANCE) {
+                                    return@thenAccept
+                                }
+
+                                // Open notification channel
+                                createNotificationChannel(globalContext)
+                                val intent = Intent(
+                                    globalContext,
+                                    HelperPageActivity::class.java
+                                )
+
                                 // Data to transfer to the help page activity
                                 val bundle = Bundle()
                                 bundle.putString(EXTRA_EMERGENCY_KEY, it.id)
@@ -44,10 +71,11 @@ object EmergencyListener {
                                 bundle.putDouble(EXTRA_DESTINATION_LAT, it.latitude)
                                 bundle.putDouble(EXTRA_DESTINATION_LONG, it.longitude)
                                 intent.putExtras(bundle)
-                                NotificationService.sendIntentNotification(
-                                    SignInActivity.globalContext,
-                                    SignInActivity.globalContext.getString(R.string.emergency),
-                                    SignInActivity.globalContext.getString(R.string.need_help),
+
+                                sendIntentNotification(
+                                    globalContext,
+                                    globalContext.getString(R.string.emergency),
+                                    globalContext.getString(R.string.need_help),
                                     intent
                                 )
                             }
@@ -55,11 +83,26 @@ object EmergencyListener {
                 }
             }
             // Chain of listener instantiations
-            activateListener(skills.hasVentolin, SignInActivity.globalContext.getString(R.string.asthma_med))
-            activateListener(skills.isMedicalPro, SignInActivity.globalContext.getString(R.string.med_pro))
-            activateListener(skills.hasEpipen, SignInActivity.globalContext.getString(R.string.epipen))
-            activateListener(skills.hasInsulin, SignInActivity.globalContext.getString(R.string.Insulin))
-            activateListener(skills.hasFirstAidKit, SignInActivity.globalContext.getString(R.string.first_aid_kit))
+            activateListener(
+                skills.hasVentolin,
+                SignInActivity.globalContext.getString(R.string.asthma_med)
+            )
+            activateListener(
+                skills.isMedicalPro,
+                SignInActivity.globalContext.getString(R.string.med_pro)
+            )
+            activateListener(
+                skills.hasEpipen,
+                SignInActivity.globalContext.getString(R.string.epipen)
+            )
+            activateListener(
+                skills.hasInsulin,
+                SignInActivity.globalContext.getString(R.string.Insulin)
+            )
+            activateListener(
+                skills.hasFirstAidKit,
+                SignInActivity.globalContext.getString(R.string.first_aid_kit)
+            )
             activateListener(skills.knowsCPR, SignInActivity.globalContext.getString(R.string.cpr))
         }
     }
