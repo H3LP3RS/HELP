7fec1a4bfb27bb82c615cfcf3d3d8f53108f0c99
Hind <hind.elbouchrifi@gmail.com>
2022-05-17 23:22:51 +0200

    Add rating backend

--- app/src/main/java/com/github/h3lp3rs/h3lp/RatingActivity.kt
+++ app/src/main/java/com/github/h3lp3rs/h3lp/RatingActivity.kt
@@ -1,11 +1,42 @@
 package com.github.h3lp3rs.h3lp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
+import android.widget.TextView
+import com.github.h3lp3rs.h3lp.database.Databases
+import com.github.h3lp3rs.h3lp.dataclasses.Rating
 
 class RatingActivity : AppCompatActivity() {
+    private lateinit var ratingBar: RatingBar
+    private lateinit var value: TextView
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rating)
+
+        ratingBar = findViewById(R.id.rating_bar)
+        value = findViewById(R.id.value)
+        value.text = "Value: " + ratingBar.rating
+
+        ratingBar.onRatingBarChangeListener =
+            RatingBar.OnRatingBarChangeListener{ _, rating, _ ->
+                value.text = "Value: $rating"
+            }
+
+        findViewById<Button>(R.id.send_feedback_button).setOnClickListener { sendFeedback() }
+
+    }
+
+    private fun sendFeedback(){
+        val comment = findViewById<EditText>(R.id.comment).text.toString()
+        val rating = Rating(ratingBar.rating, comment)
+        Databases.databaseOf(Databases.RATINGS).setObject(System.currentTimeMillis().toString(),Rating::class.java,rating)
     }
 }
+
+
---
+++ app/src/main/java/com/github/h3lp3rs/h3lp/dataclasses/Rating.kt
@@ -0,0 +1,6 @@
+package com.github.h3lp3rs.h3lp.dataclasses
+
+data class Rating(
+    val value: Float,
+    val comment: String
+)
