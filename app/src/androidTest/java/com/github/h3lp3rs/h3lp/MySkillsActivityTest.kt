f5e15e497e3964b1f9016f0eb68ceb278f61ddf7
Adrinels <56875743+Adrinels@users.noreply.github.com>
2022-05-24 09:54:06 +0200

    add load/save test

--- app/src/androidTest/java/com/github/h3lp3rs/h3lp/MySkillsActivityTest.kt
+++ app/src/androidTest/java/com/github/h3lp3rs/h3lp/MySkillsActivityTest.kt
@@ -1,5 +1,6 @@
 package com.github.h3lp3rs.h3lp
 
+import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.*
@@ -15,8 +16,11 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.h3lp3rs.h3lp.database.Databases.*
 import com.github.h3lp3rs.h3lp.database.Databases.Companion.setDatabase
 import com.github.h3lp3rs.h3lp.database.MockDatabase
+import com.github.h3lp3rs.h3lp.dataclasses.HelperSkills
+import com.github.h3lp3rs.h3lp.dataclasses.MedicalType
 import com.github.h3lp3rs.h3lp.signin.SignInActivity.Companion.globalContext
 import com.github.h3lp3rs.h3lp.signin.SignInActivity.Companion.userUid
+import com.github.h3lp3rs.h3lp.storage.Storages
 import com.github.h3lp3rs.h3lp.storage.Storages.Companion.resetStorage
 import org.hamcrest.Matchers.*
 import org.junit.Before
@@ -26,9 +30,12 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MySkillsActivityTest : H3lpAppTest() {
 
+    val ctx:Context = getApplicationContext()
+
     private fun launch(): ActivityScenario<MySkillsActivity> {
         return launch(Intent(getApplicationContext(), MySkillsActivity::class.java))
     }
+
 
     @Before
     fun dataInit() {
@@ -37,12 +44,18 @@ class MySkillsActivityTest : H3lpAppTest() {
 
         setDatabase(PREFERENCES, MockDatabase())
         resetStorage()
+
     }
+
 
     @Test
     fun backButtonWorks() {
         launch().use {
             initIntentAndCheckResponse()
+
+            //notification hide the button so just make sure that the notification is gone to click on it
+            Thread.sleep(2000)
+
             onView(withId(R.id.mySkillsBackButton)).perform(click())
 
             intended(allOf(hasComponent(MainPageActivity::class.java.name)))
@@ -50,6 +63,24 @@ class MySkillsActivityTest : H3lpAppTest() {
             release()
         }
     }
+
+
+    @Test
+    fun loadDataWork(){
+        val skills = HelperSkills(true,true,false,false,false,false)
+        val storage = Storages.storageOf(Storages.SKILLS)
+
+        storage.setObject(ctx.getString(R.string.my_skills_key), HelperSkills::class.java, skills)
+        storage.push()
+        launch().use{
+            onView(withId(R.id.epipenSwitch)).check(ViewAssertions.matches(isChecked()))
+            onView(withId(R.id.ventolinSwitch)).check(ViewAssertions.matches(isChecked()))
+            onView(withId(R.id.insulinSwitch)).check(ViewAssertions.matches(isNotChecked()))
+        }
+
+    }
+
+
 
     @Test
     fun clickingOnHelpDisplayDialogue() {
