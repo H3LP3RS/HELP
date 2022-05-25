7a3e9e3fd58feced8ddfb44a036d81ba9ec6afab
Ahayderi <alex.hayderi@gmail.com>
2022-05-25 18:03:40 +0200

    Merge multiple firstAid activities into one

---
+++ app/src/main/java/com/github/h3lp3rs/h3lp/firstaid/FirstAidHowTo.kt
@@ -0,0 +1,4 @@
+package com.github.h3lp3rs.h3lp.firstaid
+
+enum class FirstAidHowTo {
+}
---
+++ app/src/main/java/com/github/h3lp3rs/h3lp/firstaid/GeneralFirstAidActivity.kt
@@ -0,0 +1,11 @@
+package com.github.h3lp3rs.h3lp.firstaid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GeneralFirstAidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_general_first_aid)
+    }
+}
