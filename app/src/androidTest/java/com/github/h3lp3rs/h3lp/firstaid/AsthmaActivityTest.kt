6b941228a570f31127e090653a9e94a8943abd37
Emilien Duc <56789637+emduc@users.noreply.github.com>
2022-05-30 16:39:55 +0200

    287 - Reintroduce previously deleted test classes (#288)
    * Adds HelpeeSelection tests
    
    * Launch explicit intent in tests
    
    * comment out one test
    
    * remove phone call tests
    
    * add all AwaitHelp tests
    
    * remove call tests in awaitHelp
    
    * add popup tests
    
    * adds tests for all tutorials in awaitHelp
    
    * remove scrollTo in non-scrollable layout
    
    * remove asthma test because not in layout
    
    * disable default actionBar in dark mode
    
    * Test call in helpeeSelection with mock only
    
    * Test of localStorage for cirrus
    
    * Revert change
    
    * Revert change
    
    * Add last call tests in AwaitHelp
    
    * Remove cirrus-unfriendly commented tests
    
    * Address review comments
    
    * Fix new use of plural string
    
    * Remove failing test that is refactored by Alexandre
    
    * Refactor unused conditional statement

--- app/src/androidTest/java/com/github/h3lp3rs/h3lp/firstaid/AsthmaActivityTest.kt
+++ app/src/androidTest/java/com/github/h3lp3rs/h3lp/firstaid/AsthmaActivityTest.kt
@@ -1,10 +1,13 @@
 package com.github.h3lp3rs.h3lp.firstaid
 
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.RootMatchers.isFocusable
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -28,9 +31,10 @@ class AsthmaActivityTest {
      * @param id Id of the component
      */
     private fun checkIfDisplayed(id: Int){
-        Espresso.onView(ViewMatchers.withId(id))
+        onView(ViewMatchers.withId(id)).inRoot(isFocusable())
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
+
     @Test
     fun tutorialVideoIsDisplayed(){
         checkIfDisplayed(R.id.asthmaVideo)
@@ -43,16 +47,16 @@ class AsthmaActivityTest {
         checkIfDisplayed(R.id.asthmaTutorialStep3)
     }
 
-    @Test
-    fun backButtonWorks(){
-        Intents.init()
-        Espresso.onView(ViewMatchers.withId(R.id.asthma_back_button))
-            .perform(ViewActions.scrollTo(), ViewActions.click())
-        Intents.intended(
-            Matchers.allOf(
-                IntentMatchers.hasComponent(FirstAidActivity::class.java.name)
-            )
-        )
-        Intents.release()
-    }
+//    @Test
+//    fun backButtonWorks(){
+//        Intents.init()
+//        onView(ViewMatchers.withId(R.id.asthma_back_button))
+//            .perform(ViewActions.scrollTo(), ViewActions.click())
+//        Intents.intended(
+//            Matchers.allOf(
+//                IntentMatchers.hasComponent(FirstAidActivity::class.java.name)
+//            )
+//        )
+//        Intents.release()
+//    }
 }
